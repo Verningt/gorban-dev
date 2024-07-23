394b6f079291011d4e2f6edccacd3f62e89ce797
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-23 02:26:13 -0400

    dsjnds

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import "../styles/components/header.css";
 import "../styles/components/slider.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
+import "../styles/components/contact.css";
 import "../styles/utils.css";
