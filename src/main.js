28c4c53c0163ef563891b307fb764e6b2a8d701d
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-24 16:59:37 -0400

    dkhbdshbgd

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
-// import "../styles/bootstrap-5.3.3-dist/css/bootstrap.min.css";
 import "../styles/components/header.css";
 import "../styles/components/slider.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/gallery.css";
-// import "../styles/components/contact.css";
 import "../styles/utils.css";
