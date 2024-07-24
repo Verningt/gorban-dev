7ac32341f9845780b803debb5ba79b21bd2ed3c4
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-24 16:13:05 -0400

    skdkdsf

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import "../styles/components/header.css";
 import "../styles/components/slider.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
-import "../styles/components/swiper.css";
+import "../styles/components/gallery.css";
 import "../styles/components/contact.css";
 import "../styles/utils.css";
