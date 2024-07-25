1ea909613ae9653e0d0e8843066473aa553a5f62
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-25 02:50:43 -0400

    hhjhbvh

--- src/app.js
+++ src/app.js
@@ -13,11 +13,22 @@ document
   });
 
 function copyToClipboard(text) {
-  var tempInput = document.createElement("input");
-  tempInput.value = text;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempInput);
-  alert("Скопировано: " + text);
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard
+      .writeText(text)
+      .then(function () {
+        alert("Скопировано: " + text);
+      })
+      .catch(function (err) {
+        console.error("Не удалось скопировать текст: ", err);
+      });
+  } else {
+    var tempInput = document.createElement("input");
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("Скопировано: " + text);
+  }
 }
