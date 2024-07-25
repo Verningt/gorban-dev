f55ca57b19f8e07d37d394cca436bb53b8900f13
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-25 03:11:31 -0400

    fhjfgjfjfg

--- src/app.js
+++ src/app.js
@@ -1,59 +1,18 @@
-document
-  .getElementById("copyEmail")
-  .addEventListener("click", async function (event) {
-    event.preventDefault();
-    await copyToClipboard("gortrans@live.com");
-  });
-document
-  .getElementById("copyEmail")
-  .addEventListener("touchend", async function (event) {
-    event.preventDefault();
-    await copyToClipboard("gortrans@live.com");
-  });
+document.getElementById("copyEmail").addEventListener("click", function () {
+  copyToClipboard("gortrans@live.com");
+});
 
-document
-  .getElementById("copyPhone")
-  .addEventListener("click", async function (event) {
-    event.preventDefault();
-    await copyToClipboard("+(314)-504-7886");
-  });
-document
-  .getElementById("copyPhone")
-  .addEventListener("touchend", async function (event) {
-    event.preventDefault();
-    await copyToClipboard("+(314)-504-7886");
-  });
+document.getElementById("copyPhone").addEventListener("click", function () {
+  copyToClipboard("+(314)-504-7886");
+});
 
-async function copyToClipboard(text) {
-  if (navigator.clipboard && navigator.clipboard.writeText) {
-    try {
-      await navigator.clipboard.writeText(text);
-      alert("Скопировано: " + text);
-    } catch (err) {
-      console.error("Не удалось скопировать текст: ", err);
-      fallbackCopyTextToClipboard(text);
-    }
-  } else {
-    fallbackCopyTextToClipboard(text);
-  }
-}
-
-function fallbackCopyTextToClipboard(text) {
+function copyToClipboard(text) {
   var tempInput = document.createElement("textarea");
   tempInput.value = text;
-  tempInput.style.position = "fixed"; // Avoid scrolling to bottom of page in MS Edge.
-  tempInput.style.top = "0";
-  tempInput.style.left = "0";
-  tempInput.style.width = "1px";
-  tempInput.style.height = "1px";
-  tempInput.style.padding = "0";
-  tempInput.style.border = "none";
-  tempInput.style.outline = "none";
-  tempInput.style.boxShadow = "none";
-  tempInput.style.background = "transparent";
   document.body.appendChild(tempInput);
-  tempInput.focus();
   tempInput.select();
+  tempInput.setSelectionRange(0, 99999); // For mobile devices
+
   try {
     var successful = document.execCommand("copy");
     var msg = successful ? "успешно" : "неуспешно";
@@ -61,5 +20,6 @@ function fallbackCopyTextToClipboard(text) {
   } catch (err) {
     console.error("Не удалось скопировать текст: ", err);
   }
+
   document.body.removeChild(tempInput);
 }
