3be35fb4752300f5140732eafab6d8d94bcb1b33
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-25 17:20:24 -0400

    hhbkjn

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
-  if (window.scrollY > lastScrollY) {
-    // Scrolling down
+  if (window.scrollY > lastScrollY && window.scrollY > 50) {
+    // Скрываем хедер при прокрутке вниз, если прокрутили больше 50px
     header.classList.add("hidden");
   } else {
-    // Scrolling up
+    // Показываем хедер при прокрутке вверх
     header.classList.remove("hidden");
   }
   lastScrollY = window.scrollY;
