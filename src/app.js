e8c8517fda6499ed53b1173f4699620ba8afe935
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-27 17:52:26 -0400

    kdshbfbjdsnf

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-let lastScrollY = window.scrollY;
+// let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-  const header = document.querySelector(".header");
-  if (window.scrollY > lastScrollY && window.scrollY > 50) {
-    // Скрываем хедер при прокрутке вниз, если прокрутили больше 50px
-    header.classList.add("hidden");
-  } else {
-    // Показываем хедер при прокрутке вверх
-    header.classList.remove("hidden");
-  }
-  lastScrollY = window.scrollY;
-});
+// window.addEventListener("scroll", () => {
+//   const header = document.querySelector(".header");
+//   if (window.scrollY > lastScrollY && window.scrollY > 300) {
+//     // Скрываем хедер при прокрутке вниз, если прокрутили больше 300px
+//     header.classList.add("hidden");
+//   } else if (window.scrollY < lastScrollY || window.scrollY <= 300) {
+//     // Показываем хедер при прокрутке вверх или если прокрутка меньше 300px
+//     header.classList.remove("hidden");
+//   }
+//   lastScrollY = window.scrollY;
+// });
