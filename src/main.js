f1c7269d978d2e154a81a55e05dd0d7582f6ceb9
Verningt <137651917+Verningt@users.noreply.github.com>
2024-07-28 17:16:51 -0400

    sdjgbshbg

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/contact.css";
 import "../styles/components/gallery.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
 
 !(function (e, t) {
