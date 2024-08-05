bd993de50f613a6c4d6e3176381c8b56086b10c6
Verningt <137651917+Verningt@users.noreply.github.com>
2024-08-05 18:31:50 -0400

    fdkjkgdf

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -4,17 +4,26 @@ const mobileNav = () => {
   const mobileLinks = document.querySelectorAll(".mobile-nav__link");
 
   let isMobileNavOpen = false;
+  let scrollPosition = 0;
 
   headerBtn.addEventListener("click", () => {
     isMobileNavOpen = !isMobileNavOpen;
+
     if (isMobileNavOpen) {
+      scrollPosition = window.pageYOffset;
       mobileNav.classList.add("open");
       document.body.style.overflowY = "hidden";
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${scrollPosition}px`;
+      document.body.style.width = "100%";
     } else {
       mobileNav.classList.remove("open");
       setTimeout(() => {
         document.body.style.overflowY = "auto";
-      }, 300); // Duration should match the CSS transition duration
+        document.body.style.position = "";
+        document.body.style.top = "";
+        window.scrollTo(0, scrollPosition); // Возвращаемся на исходную позицию страницы
+      }, 300); // Длительность должна совпадать с CSS transition
     }
   });
 
@@ -24,7 +33,10 @@ const mobileNav = () => {
       mobileNav.classList.remove("open");
       setTimeout(() => {
         document.body.style.overflowY = "auto";
-      }, 300); // Duration should match the CSS transition duration
+        document.body.style.position = "";
+        document.body.style.top = "";
+        window.scrollTo(0, scrollPosition); // Возвращаемся на исходную позицию страницы
+      }, 300); // Длительность должна совпадать с CSS transition
     });
   });
 };
